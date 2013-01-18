eb9ab365fff4e134acb0c51382045edf2a3a2a57
Marco Battisti <icarus@MacBook-Pro-di-Marco.local>
2013-01-18 18:53:37 +0100

    Initial Commit

---
+++ SPEA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPEA
+//
+//  Created by Marco Battisti on 18/01/13.
+//  Copyright (c) 2013 Icarus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
